2530ba6ef2f1d849b5e74866e56a09b97afd1369
marianagchavez <mariana_g_chavez@hotmail.com>
2022-05-16 23:48:40 -0300

    cambio nuevo

--- saludo.js
+++ saludo.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
 console.log ("hola, cuando nos vamos a dormir?")
-=======
-console.log ("hola mirate esto")
-
-//cleito es de macarena, sando es de las dos.
->>>>>>> 2dec22c0c73b530e15d58013277e3d9a219d4861
